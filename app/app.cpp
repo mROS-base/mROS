4f3ca5d646caad29867c81c85a53622dd7832746
MoriTomoya <mori@lab3.kuis.kyoto-u.ac.jp>
2017-11-19 14:41:20 +0900

    local modification

--- app/app.cpp
+++ app/app.cpp
@@ -11,6 +11,78 @@ static SoftPWM ledr(P6_13);                                     // LED-Red
 static SoftPWM ledg(P6_14);                                     // LED-Green
 static SoftPWM ledb(P6_15);                                     // LED-Blue
 
+//超音波センサHC-SR04の関数
+//参考（https://developer.mbed.org/users/haru36rr/notebook/hcsr04_operation_check/）
+static DigitalOut USSTriger (P2_14);         //P11 :超音波センサ トリガ出力
+Timer ActiveTime;
+/* 割り込み処理宣言 */
+Ticker TrigerTiming;                //Trigerピン :インターバルタイマ
+static InterruptIn USSEcho (P2_15);          //p12 :超音波センサ  エコー入力
+unsigned short USSDistance;         //USSDistance:超音波センサ測定距離
+static DigitalIn Button (P6_0);		//ユーザボタン
+
+void Triger (){
+    USSTriger = 1;
+    wait_us(10);
+    USSTriger = 0;
+}
+
+void RiseEcho(){
+    ActiveTime.start();
+}
+
+void FallEcho(){
+    unsigned long ActiveWidth;
+    ActiveTime.stop();
+    ActiveWidth = ActiveTime.read_us();
+    USSDistance = ActiveWidth * 0.0170;
+    ActiveTime.reset();
+}
+
+void init(void){
+    TrigerTiming.attach( Triger , 0.060 );      //USSTriger周期 60ms
+    USSEcho.rise( RiseEcho );                   //USSEcho立ち上がり時割り込み
+    USSEcho.fall( FallEcho );                   //USSEcho立ち下がり時割り込み
+}
+
+/*****mROS user task code*******/
+void usr_task1(){
+#ifndef _USR_TASK_1_
+#define _USR_TASK_1_
+
+	syslog(LOG_NOTICE,"========Activate user task1========");
+	int argc = 0;
+	char *argv = NULL;
+	ros::init(argc,argv,"mros_node");
+	ros::NodeHandle n;
+	ros::Publisher chatter_pub = n.advertise("mros_msg", 1);
+	ros::Rate loop_rate(5);
+#endif
+
+	char msg[100];
+	int count=0;
+	init();
+	bool b = false;
+	bool bb = true;
+	syslog(LOG_NOTICE,"Data Publish Start");
+	while(1){
+		if(Button.read() == 0 && bb){
+			b = !b;
+			bb = false;
+		}else if(Button.read() == 1){
+			bb = true;
+		}
+		if(b){
+			wait_ms(1000);
+			sprintf(msg,"Distance[%d]cm\0",USSDistance);
+			chatter_pub.publish(msg);
+			loop_rate.sleep();
+		}
+	}
+}
+
+
+/******* LED for mbed library　*******/
 void led_init(){
 	ledu = 0;
 	ledr.period_ms(10);
@@ -50,39 +122,13 @@ void LED_switch(string *msg){
 	}
 }
 
-
-
+/*******  callback **********/
 void Callback(string *msg){	
 	LED_switch(msg);
 	syslog(LOG_NOTICE,"I heard [%s]",msg->c_str());
 }
 
-
-void usr_task1(){
-#ifndef _USR_TASK_1_
-#define _USR_TASK_1_
-
-	syslog(LOG_NOTICE,"========Activate user task1========");
-	int argc = 0;
-	char *argv = NULL;
-	ros::init(argc,argv,"mros_node");
-	ros::NodeHandle n;			
-	ros::Publisher chatter_pub = n.advertise("mros_msg", 1);
-	ros::Rate loop_rate(5);
-#endif
-
-	char str[100];
-	int count=0;
-	syslog(LOG_NOTICE,"Data Publish Start");
-	while(1){
-		sprintf(str,"mROS Hello!! [%d]",count);
-		chatter_pub.publish(str);
-		count++;
-		loop_rate.sleep();
-	}
-
-}
-
+/*****mROS user task code*******/
 void usr_task2(){
 
 #ifndef _USR_TASK_2_
