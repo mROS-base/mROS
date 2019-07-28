dc6d70f4abeb500b30388d29e6ad745ccbc2f444
Hidetoshi Yugen <hide.delorean@gmail.com>
2019-07-29 01:58:12 +0900

    edit: mros_srcを消去

--- mros_src/camera_app/message_class_specialization.h
+++
@@ -1,9 +0,0 @@
-#include "ros.h"
-
-#include "std_msgs/String.h"
-
-
-
-template ros::Subscriber ros::NodeHandle::subscribe(std::string,int,void (*fp)(std_msgs::String*));
-template ros::Publisher ros::NodeHandle::advertise<std_msgs::String>(string, int);
-template void ros::Publisher::publish(std_msgs::String&);
