e8f7ae01220ee26d695339e77db31466ff3e44c6
tmori@esm.co.jp <tmori@esm.co.jp>
2019-08-26 10:19:33 +0900

    add target depcodes for mros comm

--- mros-lib/mros-src/comm/cimpl/target/lwip/mros_comm_cimpl.c
+++ mros-lib/mros-src/comm/cimpl/target/lwip/mros_comm_cimpl.c
@@ -17,8 +17,31 @@
  */
 #include "mros_types.h"
 #include "mros_comm_cimpl.h"
+#ifndef TARGET_ATHRILL
+#include <kernel.h>
+#include <t_syslog.h>
+#include <t_stdlib.h>
+#include "syssvc/serial.h"
+#include "syssvc/syslog.h"
+
+#include "kernel_cfg.h"
+#include "syssvc/logtask.h"
+
+#include "lwip/inet.h"
+#include "lwip/netif.h"
+#include "netif/etharp.h"
+#include "lwip/dhcp.h"
+#include "eth_arch.h"
+#include "lwip/tcpip.h"
+/*
+ * mbed_interface.h
+ */
+extern void mbed_mac_address(char *mac);
+#endif /* TARGET_ATHRILL */
+
 #include <string.h>
 #include <stdio.h>
+
 
 void mros_comm_inet_local_sockaddr_init(mRosSockAddrInType *addr, mros_int32 port)
 {
@@ -98,10 +121,161 @@ void mros_comm_timeval_set(mros_uint32 sec, mros_uint32 usec, mRosTimeValType *t
 	return;
 }
 
+#ifndef TARGET_ATHRILL
+#define USE_DHCP (0)
+#if(USE_DHCP == 0)
+	#define IP_ADDRESS  	("127.0.0.1")	/*IP address */
+	#define SUBNET_MASK		("255.0.0.0")	/*Subset mask */
+	#define DEFAULT_GATEWAY	("")	/*Default gateway */
+#endif
+
+static mros_boolean use_dhcp;
+static char mac_addr[19];
+static char ip_addr[17] = "\0";
+static char gateway[17] = "\0";
+static char networkmask[17] = "\0";
+/* TCP/IP and Network Interface Initialisation */
+static struct netif netif;
+
+
+static void tcpip_init_done(void *arg) {
+	//    tcpip_inited.release();
+	sig_sem(TCPIP_INITED);
+}
+
+static void netif_link_callback(struct netif *netif) {
+    if (netif_is_link_up(netif)) {
+        // netif_linked.release();
+		sig_sem(NETIF_LINKED);
+    }
+}
+
+static void netif_status_callback(struct netif *netif) {
+    if (netif_is_up(netif)) {
+        strcpy(ip_addr, inet_ntoa(netif->ip_addr));
+        strcpy(gateway, inet_ntoa(netif->gw));
+        strcpy(networkmask, inet_ntoa(netif->netmask));
+        // netif_up.release();
+		sig_sem(NETIF_UP);
+    }
+}
+
+static void init_netif(ip_addr_t *ipaddr, ip_addr_t *netmask, ip_addr_t *gw) {
+    tcpip_init(tcpip_init_done, NULL);
+	// tcpip_inited.wait();
+	wai_sem(TCPIP_INITED);
+
+    memset((void*) &netif, 0, sizeof(netif));
+    netif_add(&netif, ipaddr, netmask, gw, NULL, eth_arch_enetif_init, tcpip_input);
+    netif_set_default(&netif);
+
+    netif_set_link_callback  (&netif, netif_link_callback);
+    netif_set_status_callback(&netif, netif_status_callback);
+}
+
+static void set_mac_address(void) {
+#if (MBED_MAC_ADDRESS_SUM != MBED_MAC_ADDR_INTERFACE)
+    snprintf(mac_addr, 19, "%02x:%02x:%02x:%02x:%02x:%02x", MBED_MAC_ADDR_0, MBED_MAC_ADDR_1, MBED_MAC_ADDR_2,
+             MBED_MAC_ADDR_3, MBED_MAC_ADDR_4, MBED_MAC_ADDR_5);
+#else
+    char mac[6];
+    mbed_mac_address(mac);
+    snprintf(mac_addr, 19, "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+#endif
+}
+
+#if (USE_DHCP == 1)
+static int ethernet_init(void)
+{
+    use_dhcp = true;
+    set_mac_address();
+    init_netif(NULL, NULL, NULL);
+    return 0;
+}
+#endif
+
+static int ethernet_ip_init(const char* ip, const char* mask, const char* gateway)
+{
+    use_dhcp = false;
+
+    set_mac_address();
+    strcpy(ip_addr, ip);
+
+    ip_addr_t ip_n, mask_n, gateway_n;
+    inet_aton(ip, &ip_n);
+    inet_aton(mask, &mask_n);
+    inet_aton(gateway, &gateway_n);
+    init_netif(&ip_n, &mask_n, &gateway_n);
+
+    return 0;
+}
+
+static char* ethernet_getMACAddress() {
+    return mac_addr;
+}
+
+static char* ethernet_getIPAddress() {
+    return ip_addr;
+}
+
+static char* ethernet_getGateway() {
+    return gateway;
+}
+
+static char* ethernet_getNetworkMask() {
+    return networkmask;
+}
+static int ethernet_connect(unsigned int timeout_ms) {
+    eth_arch_enable_interrupts();
+
+    int inited;
+    if (use_dhcp) {
+        dhcp_start(&netif);
+        // Wait for an IP Address
+        // -1: error, 0: timeout
+		// inited = netif_up.wait(timeout_ms);
+		inited = twai_sem(NETIF_UP, timeout_ms);
+    } else {
+        netif_set_up(&netif);
+        // Wait for the link up
+        // inited = netif_linked.wait(timeout_ms);
+		inited = twai_sem(NETIF_LINKED, timeout_ms);
+    }
+	return (inited == E_OK) ? (0) : (-1);
+	//    return (inited > 0) ? (0) : (-1);
+}
+
+static void network_init(){
+#if (USE_DHCP == 1)
+	if(ethernet_init() != 0) {
+#else
+	if(ethernet_ip_init(IP_ADDRESS, SUBNET_MASK, DEFAULT_GATEWAY) != 0) {
+#endif
+		syslog(LOG_NOTICE, "Network Initialize Error\r\n");
+	return;
+	}
+		syslog(LOG_NOTICE, "Network Initialized successfully");
+	while (ethernet_connect(5000) != 0){
+		syslog(LOG_NOTICE, "LOG_NOTICE: Network Connect Error");
+	}
+	syslog(LOG_NOTICE,"MAC Address is %s\r\n", ethernet_getMACAddress());
+	syslog(LOG_NOTICE,"IP Address is %s\r\n", ethernet_getIPAddress());
+	syslog(LOG_NOTICE,"NetMask is %s\r\n", ethernet_getNetworkMask());
+	syslog(LOG_NOTICE,"Gateway Address is %s\r\n", ethernet_getGateway());
+	return;
+}
+#else
+static void network_init(void)
+{
+	lwip_init();
+	return;
+}
+#endif /* TARGET_ATHRILL */
+
 
 void mros_comm_init(void)
 {
-	lwip_init();
+	network_init();
 	return;
 }
 mros_int32 mros_comm_accept(mros_int32 s, mRosSockAddrType *addr, mRosSizeType *addrlen)
